db907f845af0d6b6d949fb29b75514be34e5fff1
shallowravine <shallowravine@gmail.com>
2023-02-10 16:44:47 -0500

    functional push

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shallowravine/Workspace_1.git
+
+go 1.18
+
+require github.com/shallowravine/Workspace_1 v0.0.0-20230210213659-f14f94e0e4ca
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	H "github.com/shallowravine/Workspace_1/helper"
 )
 
 func main() {
 	a, b := 2, 3
-	c := a + b
+	c := H.Add(a, b)
 	fmt.Println(c)
 }
